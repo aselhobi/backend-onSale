cd0a9943b0db588cc5b48f27c73247d728c8d62e
ASEL BEKBOLOT KYZY <aselhobi02@gmail.com>
2024-05-29 08:22:03 +0200

    final commit

--- jobs.js
+++ jobs.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const mongoConnectionString = 'mongodb://127.0.0.1/agenda';
+const mongoConnectionString = "mongodb://127.0.0.1/agenda";
 
 mongoose.connect(mongoConnectionString, {
   useNewUrlParser: true,
@@ -27,7 +27,7 @@ const productSchema = new mongoose.Schema({
   shop: String,
 });
 
-const Product = mongoose.model('Product', productSchema);
+const Product = mongoose.model("Product", productSchema);
 
 // app.use(bodyParser.json()); // for parsing application/json
 // app.use(cors());
@@ -99,7 +99,6 @@ async function scrapeHM() {
   await page.goto("https://www2.hm.com/en_gb/sale/ladies/view-all.html", {
     waitUntil: "domcontentloaded",
   });
-
   try {
     await page.waitForSelector("#onetrust-accept-btn-handler", {
       timeout: 3000,
@@ -110,27 +109,31 @@ async function scrapeHM() {
       "Cookie acceptance dialog did not appear for Bershka, continuing..."
     );
   }
-  await page.waitForSelector(".c04eed.ac3d9e.b19650");
 
+  
+  //scrolling function to make sure all products loaded
   await page.evaluate(async () => {
-    const distance = 1000; // distance to scroll
-    const delay = 50; // delay in ms
+    const distance = 500; 
+    const delay = 2000; 
     document.scrollingElement.scrollBy(0, distance);
     await new Promise((resolve) => setTimeout(resolve, delay));
   });
-
+  await page.waitForSelector(".c04eed.ac3d9e.b19650");
+  //await page.waitForSelector("img");
 
   const products = await page.evaluate(() => {
     return Array.from(document.querySelectorAll(".f0cf84"))
-      .slice(0, 5)
+      .slice(0, 7)
       .map((element) => {
         const linkElement = element.querySelector(".db7c79");
         const imageElement = element.querySelector("img");
+        const nameElement = element.querySelector('.d1cd7b.a09145.e07e0d.a04ae4')
+
 
         return {
           url: linkElement ? linkElement.href : null,
           imageUrl: imageElement ? imageElement.src : null,
-          name: imageElement ? imageElement.alt : null,
+          name: nameElement ? nameElement.innerText : null,
           priceOld: element.querySelector(".c04eed.ac3d9e.b19650")
             ? element.querySelector(".c04eed.ac3d9e.b19650").innerText
             : null,
@@ -144,8 +147,6 @@ async function scrapeHM() {
   await browser.close();
   return products;
 }
-
-/////////////////////////////////////////////////////
 
 async function scrapeZaraOnSale() {
   const browser = await createBrowser();
@@ -172,8 +173,8 @@ async function scrapeZaraOnSale() {
     window.scrollBy(0, window.innerHeight);
   });
   await page.evaluate(async () => {
-    const distance = 1000; // distance to scroll
-    const delay = 50; // delay in ms
+    const distance = 10000; // distance to scroll
+    const delay = 5000; // delay in ms
     document.scrollingElement.scrollBy(0, distance);
     await new Promise((resolve) => setTimeout(resolve, delay));
   });
@@ -194,12 +195,20 @@ async function scrapeZaraOnSale() {
             ? imageElement.src.replace(/w=\d+/, "w=660")
             : null,
           name: captionElement ? captionElement.innerText : null,
-          priceOld: element.querySelector(".price-old__amount.price__amount.price__amount-old > .money-amount.price-formatted__price-amount > .money-amount__main")
-              ? element.querySelector(".price-old__amount.price__amount.price__amount-old > .money-amount.price-formatted__price-amount > .money-amount__main").innerText
-              : null,
-            priceNew: element.querySelector(".price-current__amount > .money-amount.price-formatted__price-amount > .money-amount__main")
-              ? element.querySelector(".price-current__amount > .money-amount.price-formatted__price-amount > .money-amount__main").innerText
-              : null,
+          priceOld: element.querySelector(
+            ".price-old__amount.price__amount.price__amount-old > .money-amount.price-formatted__price-amount > .money-amount__main"
+          )
+            ? element.querySelector(
+                ".price-old__amount.price__amount.price__amount-old > .money-amount.price-formatted__price-amount > .money-amount__main"
+              ).innerText
+            : null,
+          priceNew: element.querySelector(
+            ".price-current__amount > .money-amount.price-formatted__price-amount > .money-amount__main"
+          )
+            ? element.querySelector(
+                ".price-current__amount > .money-amount.price-formatted__price-amount > .money-amount__main"
+              ).innerText
+            : null,
           shop: "Zara",
         };
       });
@@ -208,50 +217,118 @@ async function scrapeZaraOnSale() {
   return products;
 }
 
+async function scrapeNike() {
+  const browser = await createBrowser();
+  const page = await browser.newPage();
+  await configurePage(page);
 
 
-agenda.define("scrape_sm", async () => {
+  page.on('dialog', async (dialog) => {
+    cookieHandled = true;
+    console.log("Cookie dialog found!");
+    await dialog.accept();
+  });
 
-  const [zaraProducts, hmProducts, stradivariusProducts] =
-      await Promise.all([
-        scrapeZaraOnSale(),
-        scrapeHM(),
-        //scrapeStradivarius(),
-        //scrapeNike(searchQuery)
-      ]);
-    const allProducts = [...zaraProducts, ...hmProducts];
+  await page.goto("https://www.nike.com/hu/en/w/womens-sale-3yaepz5e1x6", {
+    waitUntil: "domcontentloaded",
+  });
 
+  await page.waitForSelector(".product-grid__items.css-hvew4t");
+  await page.waitForSelector(".product-card__hero-image.css-1fxh5tw");
+
+  const products = await page.evaluate(() => {
+    return Array.from(
+      document.querySelectorAll(".product-card.product-grid__card.css-1t0asop")
+    )
+      .slice(0, 10)
+      .map((element) => {
+        const linkElement = element.querySelector(
+          ".product-card__img-link-overlay"
+        );
+        const imageElement = element.querySelector(
+          ".product-card__hero-image.css-1fxh5tw"
+        );
+
+        let priceElement = element.querySelector(
+          ".product-price.hu__styling.is--current-price.css-11s12ax"
+        );
+        if (!priceElement) {
+          priceElement = element.querySelector(
+            ".product-price.is--current-price.css-1ydfahe"
+          );
+        }
+        return {
+          url: linkElement ? linkElement.href : null,
+          imageUrl: imageElement ? imageElement.src : null,
+          name: imageElement ? imageElement.alt : null,
+          priceOld: element.querySelector(".product-price.hu__styling.is--striked-out.css-0")
+            ? element.querySelector(".product-price.hu__styling.is--striked-out.css-0").innerText
+            : null,
+          priceNew: element.querySelector(".product-price.is--current-price.css-1ydfahe")
+            ? element.querySelector(".product-price.is--current-price.css-1ydfahe").innerText
+            : null,
+          shop: "Nike"
+        };
+      });
+  });
+
+
+  await browser.close();
+  return products;
+}
+
+agenda.define("scrape_pr", async () => {
+  const [zaraProducts, hmProducts] = await Promise.all([
+    scrapeZaraOnSale().catch((error)=>{
+      console.log("scraping failed for Zara", error.message);
+      return [];
+    }),
+    scrapeHM().catch((error)=>{
+      console.log("scraping failed for HM", error.message);
+      return [];
+    }),
+    // scrapeNike().catch((error)=>{
+    //   console.log("scraping failed for Nike", error.message);
+    //   return [];
+    // }),,
+  ]);
+  console.log("scraping is done");
+
+  const allProducts = [...zaraProducts, ...hmProducts];
   console.log(allProducts);
   await Product.deleteMany({});
   await Product.insertMany(allProducts);
-
-  console.log('Products have been scraped and stored in MongoDB');
-  //fs.writeFileSync("hmOnSale.json", JSON.stringify(allProducts, null, "\t"));
+  console.log("Products have been scraped and stored in MongoDB");
 });
 
 await agenda.start();
+console.log("Agenda started");
 
-await agenda.every("8 minutes", "scrape_sm");
+await agenda.every("5 days", "scrape_pr");
+console.log("Agenda executed");
 
 async function graceful() {
   await agenda.stop();
+  console.log("Agenda terminated");
   process.exit(0);
+
 }
 
 process.on("SIGTERM", graceful);
 process.on("SIGINT", graceful);
 
 
+
 // Express route to get on sale products
-app.get('/products/onSale', async (req, res) => {
-    try {
-      const products = await Product.find({});
-      res.json({ data: products });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  });
-  
-  app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-  });
+app.get("/products/onSale", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.json({ data: products });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
